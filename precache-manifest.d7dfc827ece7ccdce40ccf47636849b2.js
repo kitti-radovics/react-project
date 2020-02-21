564ca77a0be7637c5616c12387b74a6402a2800e
Radovics Kitti <radovicskitti@Radovicss-MacBook-Pro.local>
2020-02-21 15:32:18 +0100

    Updates

---
+++ precache-manifest.d7dfc827ece7ccdce40ccf47636849b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f750b6fb59714b1acb33be8091cb3c",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "7a9ee4237b60530832ae",
+    "url": "/react-project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a7e5a99356e9cbb18290",
+    "url": "/react-project/static/js/2.6886f634.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-project/static/js/2.6886f634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9ee4237b60530832ae",
+    "url": "/react-project/static/js/main.538366c9.chunk.js"
+  },
+  {
+    "revision": "fd645ceecebe09872af1",
+    "url": "/react-project/static/js/runtime-main.03384371.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
